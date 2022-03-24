2790bfba32edc61f8b65bc09d0fef29d3569aa8c
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-24 10:03:03 -0300

    feito - requisito 06

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const bollsColorContainer = document.getElementById('bolls-color-container');
 const textColorRgb = document.getElementById('rgb-color');
 const answerText = document.getElementById('answer');
-
-const colors = [];
+const resetButton = document.getElementById('reset-game');
 
 function createRandomColor() {
   const r = parseInt(Math.random() * 255, 10);
@@ -11,16 +10,22 @@ function createRandomColor() {
   return `(${r},${g},${b})`;
 }
 
-for (let index = 0; index < 6; index += 1) {
-  const bollColor = document.createElement('div');
-  bollColor.className = 'ball';
-  bollColor.style.backgroundColor = `rgb${createRandomColor()}`;
-  colors.push(bollColor.style.backgroundColor);
-  bollColor.style.border = '1px solid black';
-  bollsColorContainer.appendChild(bollColor);
-}
+function inicializaGame() {
+  const colors = [];
+  answerText.innerText = 'Escolha uma cor';
+  bollsColorContainer.innerHTML = null;
+  for (let index = 0; index < 6; index += 1) {
+    const bollColor = document.createElement('div');
+    bollColor.className = 'ball';
+    bollColor.style.backgroundColor = `rgb${createRandomColor()}`;
+    colors.push(bollColor.style.backgroundColor);
+    bollColor.style.border = '1px solid black';
+    bollsColorContainer.appendChild(bollColor);
+  }
 
-textColorRgb.innerText = colors[parseInt(Math.random() * 5, 10)];
+  textColorRgb.innerText = colors[parseInt(Math.random() * 5, 10)];
+}
+inicializaGame();
 
 bollsColorContainer.addEventListener('click', (event) => {
   if (event.target.classList.contains('ball')
@@ -30,3 +35,5 @@ bollsColorContainer.addEventListener('click', (event) => {
     answerText.innerText = 'Errou! Tente novamente!';
   }
 });
+
+resetButton.addEventListener('click', inicializaGame);
